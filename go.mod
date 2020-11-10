99e4636725b43f819617504a6bdb56e9b72fac66
Owen Salter <owen@devosmium.xyz>
2020-11-10 13:45:52 -0500

    Initial config code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hakkard-dev-team/hakkard-server
+
+go 1.15
+
+require github.com/Matt-Gleich/logoru v0.0.0-20201102142425-1966721282af
---
+++ server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	log "github.com/Matt-Gleich/logoru"
+
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+type Server struct {
+	Config ServerConfig
+}
+
+type ServerConfig struct {
+	MaxPlayers int    `json:"maxPlayesr"`
+	Bind       string `json:"bind"`
+	Name       string `json:"name"`
+}
+
+func main() {
+	log.Info("Parsing Config")
+	conf, err := parseConfig()
+	if err != nil {
+		log.Critical("Could not parse config: " + err.Error())
+		os.Exit(-1)
+	}
+	log.Debug(conf.MaxPlayers)
+}
+
+func parseConfig() (ServerConfig, error) {
+	data, err := ioutil.ReadFile("static/server.json")
+	if err != nil {
+		return ServerConfig{}, err
+	}
+	bytes := []byte(data)
+	var conf ServerConfig
+	err = json.Unmarshal(bytes, &conf)
+	if err != nil {
+		return ServerConfig{}, err
+	}
+
+	return conf, nil
+}

d3063b2a4ff3cf21e24b3eaf208d1cd2cea6d49c
Owen Salter <owen@devosmium.xyz>
2020-11-13 15:22:59 -0500

    Add look command

--- game/cmd_meta.go
+++ game/cmd_meta.go
@@ -1,6 +1,10 @@
 package game
 
-import log "github.com/Matt-Gleich/logoru"
+import (
+	"fmt"
+
+	log "github.com/Matt-Gleich/logoru"
+)
 
 func MetaCommands(router *Route) {
 	log.Debug("Meta commands...")
@@ -8,4 +12,21 @@ func MetaCommands(router *Route) {
 		ctx.Game.OnExit(*ctx.Client)
 		ctx.Client.Conn.Close()
 	}).Desc("Exits the game")
+
+	router.On("look", func(ctx *CmdContext) {
+		playerLoc, ok := ctx.Game.GetLevel(ctx.Client.Player.Location)
+		c := ctx.Client
+		if ok {
+			c.WriteLineToUser(playerLoc.Description)
+			c.WriteLineToUser("")
+			var exits = ""
+			for _, v := range playerLoc.Exits {
+				place := v.Target
+				if ok {
+					exits += fmt.Sprintf("When you look %s, you see %s", v.Direction, place)
+				}
+			}
+			c.WriteLineToUser(exits)
+		}
+	}).Desc("Looks around")
 }

442a9eec38367d0b9f17e51a0376eb051b58802e
Owen Salter <owen@devosmium.xyz>
2020-11-13 22:11:12 -0500

    Remove unneeded import statement

--- game/cmd_matchers.go
+++ game/cmd_matchers.go
@@ -1,16 +1,11 @@
 package game
 
-import (
-	"fmt"
-
-	"github.com/Matt-Gleich/logoru"
-)
 
 
 
 func NewNameMatcher(r *Route) func(string) bool {
 	return func(cmd string) bool {
-		logoru.Debug(fmt.Sprintf("Attempting to match %s to %s", r.Name, cmd))
+
 		return cmd == r.Name
 	}
 }
